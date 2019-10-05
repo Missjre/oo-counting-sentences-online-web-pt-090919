7c73f85773783470d429de47755995fee41eda69
Jalysa Reason <jalysareason@gmail.com>
2019-10-05 00:21:01 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    puts ("Hi, my name is Sophie.".sentence?)=(true)
+    "Hi, my name is Sophie.".sentence?=(true)
   end
 
   def question?
-     puts ("What's your name?".question?)=(true)
+     "What's your name?".question?)=(true)
   end
 
   def exclamation?
-     puts ("Hi, my name is Sophie!".exclamation?)=(true)
+     "Hi, my name is Sophie!".exclamation?)=(true)
   end
 
   def count_sentences
-      puts ("one. two. three?".count_sentences)=(3) 
+      "one. two. three?".count_sentences)=(3) 
   end
 end
